b5aa59a17da3d04d9259a0d45e355292ed2f9de1
mgguevara10 <miguelguevara1012@gmail.com>
2014-04-04 15:00:28 -0500

    3

--- Quiz App/JavaScript1.js
+++ Quiz App/JavaScript1.js
@@ -1,9 +1,9 @@
 ﻿var Question = {
-    Q1="Who confirmed the expansion of the universe?"
-    Q2="How long does it take light to travel from the moon to the surface of earth?"
-    Q3="NA is the official abbreviation for which element? 
-    Q4="Which of these planets does not have a satellite?"
-    Q5="An Astronomical Unit is the mean distance between which two celestial bodies?"
+    Q1:Who confirmed the expansion of the universe?,
+    Q2:How long does it take light to travel from the moon to the surface of earth?,
+    Q3:NA is the official abbreviation for which element?,
+    Q4:Which of these planets does not have a satellite?,
+    Q5=:An Astronomical Unit is the mean distance between which two celestial bodies?,
 
 Function displayQuestion(){
 
